e51ad87dcb9313388a9b8dc4cc6d7f20bc83b29e
tristan <thuckabee.work@gmail.com>
2023-01-11 19:30:36 -0600

    phase-1 done

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,7 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  if (typeof string !== 'string') {
+    throw new TypeError();
+  } else {
+    return string.split('').reverse().join('');
+  }
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,15 @@
-// Your code here
+const {expect} = require('chai');
+const reverseString = require('../problems/reverse-string');
+
+describe('reverseString()', function() {
+  it('should return the string reversed', () => {
+    expect( reverseString('fun') ).to.equal('nuf');
+  });
+  it('should throw an error for non-strings', () => {
+    let badVal = () => { reverseString(42) };
+    let goodVal =() => { reverseString('calf') };
+
+    expect( badVal ).to.throw(TypeError);
+    expect( goodVal ).to.not.throw(TypeError);
+  });
+});
